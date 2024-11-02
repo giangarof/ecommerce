31df54620a6e0f1e31924322c9e87041c9418716
Gianmarco G <giangarof98@gmail.com>
2024-11-01 22:22:09 -0400

    before production

--- front/src/components/CarouselBook.jsx
+++ front/src/components/CarouselBook.jsx
@@ -13,19 +13,22 @@ export default function CarouselBook() {
     console.log(data)
   return (
     isLoading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
-        <Carousel pause='hover' className='bg-primary mb-4'>
-            {data.map(x => (
-                <Carousel.Item key={x._id}>
-                            <Link to={`/product/${x._id}`}>
-                                <Image src={x.image[0]?.url} alt={x.name} className="image-size" />
-                            </Link>
-                   
-                                <Carousel.Caption >
-                                    <h2>{x.name}</h2>
-                                </Carousel.Caption>
-                </Carousel.Item>
-            ))}
-        </Carousel>
+      <Carousel pause='hover' className='mb-5 bg-dark'>
+          {data.map(x => (
+              <Carousel.Item key={x._id}>
+                   <Link to={`/product/${x._id}`} className='d-flex align-items-center justify-content-center'>
+                      <Image src={x.image[0]?.url} alt={x.name} className="image-size" />
+                  </Link>
+                    <Carousel.Caption className='carousel-caption' >
+                        <div className='centered'>
+
+                          <p>{x.description}</p>
+                        </div>
+                    </Carousel.Caption> 
+              </Carousel.Item>
+          ))}
+      </Carousel>
+
     )
   )
 }
